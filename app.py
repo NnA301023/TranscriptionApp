70b9d9f424abe51d0c75783fe9b73976eb5bfc8e
alif_ramadhan <alif@skinmystery.com>
2023-06-21 13:55:28 +0700

    trace: list dir error

--- app.py
+++ app.py
@@ -36,6 +36,8 @@ def get_yt(URL):
     #st.info('2. Audio file has been retrieved from YouTube video')
     bar.progress(10)
     st.success(yt.get_file_path())
+    st.success(os.path.isfile(os.path.join("static/video", yt.get_file_path().split("\\")[-1])))
+    
     return yt.get_file_path().split("\\")[-1]
 
 # 3. Upload YouTube audio file to AssemblyAI
